5693323c19e25fb160911d97fc4699cd3565219a
ReconTrapper <95002568+ReconTrapper@users.noreply.github.com>
2023-12-06 23:30:12 -0500

    Add files via upload

---
+++ Hunting_Grounds_Project/Scripts/common.py
@@ -0,0 +1,13 @@
+# common.py
+import socket
+import requests
+
+def resolve_ip(subdomain):
+    # ... (same as previous script)
+
+def check_ports(ip_address, ports):
+    # ... (same as previous script)
+
+def http_request(url):
+    # ... (same as previous script)
+
